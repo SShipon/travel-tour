ddc9388bac5384e709f1a6b5a20eb76c5a4638bc
SShipon <nxshipon@gmail.com>
2022-03-31 02:01:28 +0630

    hey

--- src/components/Homepage/Banner/Banner.js
+++ src/components/Homepage/Banner/Banner.js
@@ -1,18 +1,30 @@
 import React from "react";
 import "./Banner.css";
+import Typewriter from "typewriter-effect";
 
 const Banner = () => {
     return (
-        <div className="banner-img">
-            <div className="banner-text">
-                <h1>Your Journey Begins</h1>
-                <p>
-                    A journy of a 1000 miles start with a single steps.One’s
-                    destination is never a place, but a new way of seeing
-                    things.
-                </p>
-            </div>
+      <div className="banner-img">
+        <div className="banner-text">
+          <h2 className="type-content">
+            <Typewriter
+              options={{
+                strings: [
+                  "Hello I am Shipon ",
+                  "Mern Stack Developer",
+                  "Web Designer",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
+          <p>
+            A journy of a 1000 miles start with a single steps.One’s destination
+            is never a place, but a new way of seeing things.
+          </p>
         </div>
+      </div>
     );
 };
 
--- src/firebase/Firebase.Config.js
+++ src/firebase/Firebase.Config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCrbqpgPId_RkGTZe5vM_JhZ8zACdWZATY",
-  authDomain: "travel-projects-ea734.firebaseapp.com",
-  projectId: "travel-projects-ea734",
-  storageBucket: "travel-projects-ea734.appspot.com",
-  messagingSenderId: "621114642175",
-  appId: "1:621114642175:web:6fba4f5b54583fa46306ca",
+  apiKey: "AIzaSyDvJSUfn-taC1TTRrGVBwOrq9AEQxi-68Y",
+  authDomain: "travel-tour-5fac8.firebaseapp.com",
+  projectId: "travel-tour-5fac8",
+  storageBucket: "travel-tour-5fac8.appspot.com",
+  messagingSenderId: "600870149939",
+  appId: "1:600870149939:web:12c8adeb22644659ab2846",
 };
 
 export default firebaseConfig;
